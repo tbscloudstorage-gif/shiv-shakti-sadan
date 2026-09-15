fc384c6ea14a798ed70ad1df693d14f282017832
Harshita Srivastava <tbscloudstorage@gmail.com>
2026-09-15 15:03:06 +0530

    new changes

--- src/components/cta/CtaThree.tsx
+++ src/components/cta/CtaThree.tsx
@@ -8,26 +8,26 @@ export default function CtaThree() {
           <div className="it-stats-wrap">
             <div className="row">
               <div className="col-lg-4 col-md-4">
-                <div className="it-stats-item">
+                <div className="it-stats-item p-3">
                   <div>
-                    <h4 className="it-stats-title">52 </h4>
-                    <span>Years of educational excellence   since 1974</span>
+                    <h4 className="it-stats-title text-center">50+ </h4>
+                    <span className="text-center d-flex ">Years of educational excellence   since 1974</span>
                   </div>
                 </div>
               </div>
               <div className="col-lg-4 col-md-4">
-                <div className="it-stats-item">
+                <div className="it-stats-item p-3">
                   <div>
-                    <h4 className="it-stats-title">7</h4>
-                    <span>Learning stages from Pre-Primary onwards</span>
+                    <h4 className="it-stats-title text-center">7</h4>
+                    <span className="text-center d-flex ">Learning stages from pre primary onwards</span>
                   </div>
                 </div>
               </div>
               <div className="col-lg-4 col-md-4">
-                <div className="it-stats-item d-flex justify-content-end">
+                <div className="it-stats-item  p-3 d-flex justify-content-end">
                   <div>
-                    <h4 className="it-stats-title">25k+ </h4>
-                    <span>Students nurtured with <br/>care and values </span>
+                    <h4 className="it-stats-title text-center">25K+ </h4>
+                    <span className="text-center d-flex">Students nurtured with <br/>care and values </span>
                   </div>
                 </div>
               </div>
