9a8bd28597c8a698b18ba9e6737b11c2fcb22527
Harshita Srivastava <tbscloudstorage@gmail.com>
2026-09-16 12:26:26 +0530

    gallery page created

--- src/components/GalleryPage/index.tsx
+++ src/components/GalleryPage/index.tsx
@@ -1,19 +1,19 @@
 // import FooterFour from "@/layouts/footers/FooterFour";
 // import HeaderInner from "@/layouts/headers/HeaderInner";
 import Wrapper from "@/layouts/Wrapper";
-import AboutOne from "@/components/about/AboutOne";
+// import AboutOne from "@/components/about/AboutOne";
 // import BrandOne from "@/components/brand/BrandOne";
 import Breadcrumb from "@/components/breadcrumb/Breadcrumb";
-import ChooseFive from "@/components/choose/ChooseFive";
+// import ChooseFive from "@/components/choose/ChooseFive";
 import BackToTop from "@/components/common/BackToTop";
-import CountDownOne from "@/components/countdown/CountDownOne";
+// import CountDownOne from "@/components/countdown/CountDownOne";
 // import CtaFive from "@/components/cta/CtaFive";
 // import TeamOne from "@/components/team/TeamOne";
 // import TestimonialOne from "@/components/testimonial/TestimonialOne";
 import FooterThree from "@/layouts/footers/FooterThree";
 import HeaderOne from "@/layouts/headers/HeaderOne";
-import TestimonialThree from "@/components/testimonial/TestimonialThree";
-import ImageSection from "./ImageSection";
+// import TestimonialThree from "@/components/testimonial/TestimonialThree";
+import ImageSection from "@/components/GalleryPage/";
 
 
 
@@ -24,7 +24,7 @@ export default function Aboutusv1() {
        />
       <main>
         <Breadcrumb title="Gallery" subtitle="Gallery" breadcrumb_img="/assets/img/breadcrumb/course-v1-breadcrumb.jpg" />
-      <ImageSection style_2={true} />
+      <ImageSection/>
       </main>
       <FooterThree/>
       <BackToTop />
